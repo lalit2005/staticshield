8ccb3d19c37f92d670ddc8c0d7ccff9a389a3006
Lalit <lalitvijay9480@gmail.com>
2021-06-03 13:16:22 +0530

    refactor: add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import { Page, Text } from "@geist-ui/react";
-import Head from "next/head";
+import { Page, Text } from '@geist-ui/react';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="bg-gray-50">
+    <div className='bg-gray-50'>
       <Page>
         <Head>
           <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel='icon' href='/favicon.ico' />
         </Head>
         <Text h1>StaticShield</Text>
       </Page>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
