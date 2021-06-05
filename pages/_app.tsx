e89190514d51ce4b70da1f2a679be6e47862fbaf
Lalit <lalitvijay9480@gmail.com>
2021-06-05 14:19:50 +0530

    style: add Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '@fontsource/inter/variable.css';
 import '../styles/globals.css';
 import { GeistProvider, CssBaseline } from '@geist-ui/react';
 import { UserProvider } from '@auth0/nextjs-auth0';
