d32678966c7992acdbb56dc37176678760b69c7b
Lalit <lalitvijay9480@gmail.com>
2021-06-06 20:05:13 +0530

    style: fix sticky nav in dashboard

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -40,7 +40,7 @@ export default function DashboardNavbar(props: DashboardNavbarProps) {
   };
 
   return (
-    <div className='flex items-center justify-between px-16 mb-12 border-b border-gray-200 bg-gray-50'>
+    <div className='fixed flex items-center justify-between w-full px-16 mb-12 border-b border-gray-200 bg-gray-50'>
       <div className='flex items-center justify-between'>
         <img src='/staticshield.png' alt='StaticShield' width='25px' />
         <Text b p className='ml-2 text-2xl'>
