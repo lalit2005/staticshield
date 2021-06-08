e762e0326e80054abcbda5fa720425d368d4751e
Lalit <lalitvijay9480@gmail.com>
2021-06-08 19:57:23 +0530

    refactor: move features array to new file

---
+++ lib/features.tsx
@@ -0,0 +1,38 @@
+import { Code, Lock, Zap, Server, Clock } from '@geist-ui/react-icons';
+import Shield from '../components/Shield';
+import { NavBarProps } from '../lib/interfaces';
+
+const features: NavBarProps[] = [
+  {
+    feature:
+      "Password protect any website only with a single line of code. That's it, really!!",
+    children: <Code />,
+  },
+  {
+    feature:
+      'No database, backend or server side code is required. Everything is taken care by StaticShield',
+    children: <Server />,
+  },
+  {
+    feature:
+      'From signup to password protecting your site, it takes only 3 minutes!',
+    children: <Clock />,
+  },
+  {
+    feature:
+      'The quickest way to set up password login page, hosted on StaticShield',
+    children: <Shield />,
+  },
+  {
+    feature:
+      'StaticShield is framework agnostic. It works with the framework of your choice',
+    children: <Zap />,
+  },
+  {
+    feature:
+      'StaticShield hashes and encrypts data multiple times so that your data stays highly secure',
+    children: <Lock />,
+  },
+];
+
+export default features;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,9 @@
 import { Button, Divider, Page, Text, Link } from '@geist-ui/react';
-import { Code, Lock, Zap, Server, Clock } from '@geist-ui/react-icons';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import FeatureCard from '../components/FeatureCard';
-import Shield from '../components/Shield';
-import { NavBarProps } from '../lib/interfaces';
 import { useUser } from '@auth0/nextjs-auth0';
-
-const features: NavBarProps[] = [
-  {
-    feature:
-      "Password protect any website only with a single line of code. That's it, really!!",
-    children: <Code />,
-  },
-  {
-    feature:
-      'No database, backend or server side code is required. Everything is taken care by StaticShield',
-    children: <Server />,
-  },
-  {
-    feature:
-      'From signup to password protecting your site, it takes only 3 minutes!',
-    children: <Clock />,
-  },
-  {
-    feature:
-      'The quickest way to set up password login page, hosted on StaticShield',
-    children: <Shield />,
-  },
-  {
-    feature:
-      'StaticShield is framework agnostic. It works with the framework of your choice',
-    children: <Zap />,
-  },
-  {
-    feature:
-      'StaticShield hashes and encrypts data multiple times so that your data stays highly secure',
-    children: <Lock />,
-  },
-];
+import features from '../lib/features';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
