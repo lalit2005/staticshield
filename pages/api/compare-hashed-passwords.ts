7e1063f4b422648a0c15c6bb3c7729a24be85482
Lalit <lalitvijay9480@gmail.com>
2021-06-12 13:07:05 +0530

    refactor: remove unwanted api routes

--- pages/api/compare-hashed-passwords.ts
+++
@@ -1,9 +0,0 @@
-import compareHashedPasswords from '@/lib/compareHashedPasswords';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const pwd = req.query.pwd.toString();
-  const hash = req.query.hash.toString();
-  const h = await compareHashedPasswords(pwd, hash);
-  res.json({ isSame: h });
-}
--- pages/api/hash-password.ts
+++
@@ -1,8 +0,0 @@
-import hashPassword from '@/lib/hashPassword';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const pwd = req.query.pwd.toString();
-  const h = await hashPassword(pwd);
-  res.json({ hash: h });
-}
