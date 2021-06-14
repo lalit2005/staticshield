fcf25698a2f5992fed0733a563c37d0b5142f027
Lalit <lalitvijay9480@gmail.com>
2021-06-14 13:25:13 +0530

    feat: add page transition animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,28 @@ import '@fontsource/ibm-plex-mono/400.css';
 import '@/styles/globals.css';
 import { GeistProvider, CssBaseline } from '@geist-ui/react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <UserProvider>
-      <GeistProvider>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </GeistProvider>
+      <motion.div
+        key={router.route}
+        initial='pageInitial'
+        animate='pageAnimate'
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}>
+        <GeistProvider>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </GeistProvider>
+      </motion.div>
     </UserProvider>
   );
 }
