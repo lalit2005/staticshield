f216a0bb294e436f31fd4a2bf32ea7cc89b10999
Lalit <lalitvijay9480@gmail.com>
2021-06-21 16:11:36 +0530

    refactor: remove unwanted scripts

--- public/protect-script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const params = new URLSearchParams(window.location.search);
-  const tokenFromUrl = params.get('token');
-  if (tokenFromUrl) {
-    window.localStorage.setItem('token', tokenFromUrl);
-  }
-})();
