64cb2923df0cb7997e8be215986d92fa9a3e81b7
Lalit <lalitvijay9480@gmail.com>
2021-06-21 16:14:17 +0530

    refactor: remove debugger and console logs

--- public/script.js
+++ public/script.js
@@ -9,20 +9,11 @@
   const siteId = staticshieldsScript.getAttribute('data-site-id');
   const token = window.localStorage.getItem('token');
 
-  console.log('-------from script--------');
   if (token == null || undefined || !token) {
-    console.log(
-      `https://staticshield.vercel.app/p/?id=${siteId}&redirecturl=${window.location.href
-        .split('?')[0]
-        .toString()}`
-    );
-    debugger;
     window.location.replace(
       `https://staticshield.vercel.app/p/?id=${siteId}&redirecturl=${window.location.href
         .split('?')[0]
         .toString()}`
     );
   }
-
-  console.log('-------from script--------');
 })();
