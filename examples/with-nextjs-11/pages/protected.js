8393090c6a0b0989a7bf1a9d994a1f3ac8be804a
Lalit <lalitvijay9480@gmail.com>
2021-06-21 13:32:26 +0530

    chore(example): remove next/image and add next export option for builds

--- examples/with-nextjs-11/pages/protected.js
+++ examples/with-nextjs-11/pages/protected.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Script from 'next/script';
 
@@ -47,18 +46,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
