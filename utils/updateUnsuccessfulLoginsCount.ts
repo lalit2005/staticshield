3f5046dcc9ca01f8a079104f99cff60478fb07b1
Lalit <lalitvijay9480@gmail.com>
2021-06-22 16:40:55 +0530

    feat: add unsuccessful login count

---
+++ utils/updateUnsuccessfulLoginsCount.ts
@@ -0,0 +1,22 @@
+import { HarperDBRecord } from '@/lib/interfaces';
+import { getUnixTime } from 'date-fns';
+
+export default async function updateLoginCount(
+  siteId: string,
+  data: HarperDBRecord
+): Promise<{ success: boolean }> {
+  const res = await fetch(process.env.HARPERDB_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${process.env.HARPERDB_KEY}`,
+    },
+    body: JSON.stringify({
+      operation: 'sql',
+      sql: `update site_schema.sites set no_of_failed_logins = no_of_failed_logins + 1 where id = "${siteId}"`,
+    }),
+  });
+  if (res.ok) {
+    return { success: true };
+  }
+}
