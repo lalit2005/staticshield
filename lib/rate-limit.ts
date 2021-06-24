37c2d01772ee5eed9c2f8e61f81f40f7d17d85e7
Lalit <lalitvijay9480@gmail.com>
2021-06-24 08:42:01 +0530

    feat: add rate limiting

---
+++ lib/rate-limit.ts
@@ -0,0 +1,32 @@
+import LRU from 'lru-cache';
+import { NextApiResponse } from 'next';
+
+const rateLimit = (options) => {
+  const tokenCache = new LRU({
+    max: parseInt(options.uniqueTokenPerInterval || 500, 10),
+    maxAge: parseInt(options.interval || 60000, 10),
+  });
+
+  return {
+    check: (res: NextApiResponse, limit, token: string) =>
+      new Promise<void>((resolve, reject) => {
+        const tokenCount = tokenCache.get(token) || [0];
+        if (tokenCount[0] === 0) {
+          tokenCache.set(token, tokenCount);
+        }
+        tokenCount[0] += 1;
+
+        const currentUsage = tokenCount[0];
+        const isRateLimited = currentUsage >= parseInt(limit, 10);
+        res.setHeader('X-RateLimit-Limit', limit);
+        res.setHeader(
+          'X-RateLimit-Remaining',
+          isRateLimited ? 0 : limit - currentUsage
+        );
+
+        return isRateLimited ? reject() : resolve();
+      }),
+  };
+};
+
+export default rateLimit;
--- pages/api/login-to-site.ts
+++ pages/api/login-to-site.ts
@@ -4,75 +4,91 @@ import getHashedPasswordFromSiteId from '@/utils/getHashedPasswordFromSiteId';
 import jwt from 'jsonwebtoken';
 import updateLoginCount from '@/utils/updateLoginCount';
 import updateUnsuccessfulLoginCount from '@/utils/updateUnsuccessfulLoginsCount';
+import rateLimit from '@/lib/rate-limit';
+
+const limiter = rateLimit({
+  interval: 60 * 1000, // 60 seconds
+  uniqueTokenPerInterval: 500, // Max 500 users per second
+});
 
 const loginToSite = async (
   req: NextApiRequest,
   res: NextApiResponse<{ success: boolean; token: string; message: string }>
 ) => {
-  const siteId = req.body.siteId;
-  const password = req.body.password;
-  const siteData = await getHashedPasswordFromSiteId(siteId);
-  if (siteData[0] == undefined) {
-    res.json({
+  try {
+    await limiter.check(res, 5, 'CACHE_TOKEN'); // 5 requests per minute
+
+    const siteId = req.body.siteId;
+    const password = req.body.password;
+    const siteData = await getHashedPasswordFromSiteId(siteId);
+    if (siteData[0] == undefined) {
+      res.json({
+        success: false,
+        token: '',
+        message: 'Site not found. Invalid URL',
+      });
+      return;
+    }
+    const {
+      password_hash: passwordHash,
+      is_login_blocked: isLoginBlocked,
+      max_login_duration: maxLoginDuration,
+      max_logins: maxLogins,
+      no_of_logins: numberOfLogins,
+    } = siteData[0];
+
+    if (+numberOfLogins >= +maxLogins) {
+      res.json({
+        message:
+          'This site exceeded the maximum number of logins given per month',
+        token: '',
+        success: false,
+      });
+      return;
+    }
+
+    const isPasswordCorrect = compareHashedPasswords(
+      password.toString(),
+      passwordHash.toString()
+    );
+
+    if (!isPasswordCorrect) {
+      res.json({
+        success: false,
+        token: '',
+        message: 'The password you entered is incorrect',
+      });
+      updateUnsuccessfulLoginCount(siteId, siteData[0]);
+      return;
+    }
+    if (isLoginBlocked) {
+      res.json({
+        success: false,
+        token: '',
+        message:
+          'The login to the website has been temporarily blokced by the owner.',
+      });
+      return;
+    }
+
+    const payload = {
+      loggedIn: true,
+    };
+
+    const jwtToken = jwt.sign(payload, process.env.JWT_TOKEN, {
+      expiresIn: maxLoginDuration + 'd',
+    });
+
+    const asd = await updateLoginCount(siteId, siteData[0]);
+    console.log(asd);
+    res.json({ success: true, token: jwtToken, message: 'success' });
+  } catch (error) {
+    res.status(429).json({
       success: false,
       token: '',
-      message: 'Site not found. Invalid URL',
+      message: 'You have exceeded the rate limit. Try again later',
     });
-    return;
   }
-  const {
-    password_hash: passwordHash,
-    is_login_blocked: isLoginBlocked,
-    max_login_duration: maxLoginDuration,
-    max_logins: maxLogins,
-    no_of_logins: numberOfLogins,
-  } = siteData[0];
-
-  if (+numberOfLogins >= +maxLogins) {
-    res.json({
-      message:
-        'This site exceeded the maximum number of logins given per month',
-      token: '',
-      success: false,
-    });
-    return;
-  }
-
-  const isPasswordCorrect = compareHashedPasswords(
-    password.toString(),
-    passwordHash.toString()
-  );
-
-  if (!isPasswordCorrect) {
-    res.json({
-      success: false,
-      token: '',
-      message: 'The password you entered is incorrect',
-    });
-    updateUnsuccessfulLoginCount(siteId, siteData[0]);
-    return;
-  }
-  if (isLoginBlocked) {
-    res.json({
-      success: false,
-      token: '',
-      message:
-        'The login to the website has been temporarily blokced by the owner.',
-    });
-    return;
-  }
-
-  const payload = {
-    loggedIn: true,
-  };
-
-  const jwtToken = jwt.sign(payload, process.env.JWT_TOKEN, {
-    expiresIn: maxLoginDuration + 'd',
-  });
-
-  const asd = await updateLoginCount(siteId, siteData[0]);
-  console.log(asd);
-  res.json({ success: true, token: jwtToken, message: 'success' });
 };
 
 export default loginToSite;
