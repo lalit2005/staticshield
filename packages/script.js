8a09c52355ea92d69ddd2a5d991371471ba194da
Lalit <lalitvijay9480@gmail.com>
2021-06-27 21:12:21 +0530

    fix: decode uri in client

--- packages/script.js
+++ packages/script.js
@@ -20,7 +20,8 @@
 
   if (token) {
     const res = await fetch(
-      'https://staticshield.vercel.app/api/verify-token/?token=' + token
+      'https://staticshield.vercel.app/api/verify-token/?token=' +
+        decodeURIComponent(token)
       // 'http://localhost:3000/api/verify-token/?token=' + token
     );
     const data = await res.json();
