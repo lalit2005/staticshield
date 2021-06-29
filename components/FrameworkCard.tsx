23336f62dc0a335649d8414d9a1e2414f348c3c6
Lalit <lalitvijay9480@gmail.com>
2021-06-29 14:22:38 +0530

    style: chage shadow effects

--- components/FrameworkCard.tsx
+++ components/FrameworkCard.tsx
@@ -5,7 +5,7 @@ import NextLink from 'next/link';
 const FrameworkCard: React.FC<FrameworkCardProps> = ({ name, link, img }) => {
   return (
     <NextLink href={link}>
-      <div className='max-w-sm px-32 py-4 mx-auto my-3 transition-all duration-300 border border-gray-300 rounded-md shadow-md cursor-pointer hover:shadow-2xl'>
+      <div className='max-w-sm px-32 py-4 mx-auto my-3 transition-all duration-100 border border-gray-300 rounded-md shadow-md cursor-pointer hover:shadow-xl'>
         <div className='!w-32 mx-auto'>
           <Image src={img} alt='' placeholder='blur' />
         </div>
