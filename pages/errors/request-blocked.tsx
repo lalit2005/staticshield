cb398ee4e4de8bfb96e07d31abd7039d38019075
Lalit <lalitvijay9480@gmail.com>
2021-07-01 20:29:05 +0530

    refactor: add image

--- pages/errors/request-blocked.tsx
+++ pages/errors/request-blocked.tsx
@@ -6,19 +6,30 @@ export default function NoScriptError() {
   return (
     <div className='min-h-screen text-center'>
       <Navbar />
-      <div className='pt-32'>
-        <div className='max-w-lg mx-auto'></div>
-        <div className='max-w-lg mx-auto mt-20'>
-          <Text h3>StaticShield has to make Network requests</Text>
-          <p>
-            Staticshield checks for the valid token, verifies it and redirects
-            to login page using Javascript. All this is done by the script that
-            is loaded externally. Please enable network requests in your
-            browser.
-          </p>
+      <div className='pt-20'>
+        <div className='max-w-lg mx-auto'>
+          <div className='max-w-lg mx-auto'>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              className='mx-auto my-10'
+              src='/staticshield.png'
+              alt='StaticShield requires Javascript'
+              width='160px'
+              height='160px'
+            />
+          </div>
+          <div className='max-w-lg mx-auto'>
+            <Text h3>StaticShield has to make Network requests</Text>
+            <p>
+              Staticshield checks for the valid token, verifies it and redirects
+              to login page using Javascript. All this is done by the script
+              that is loaded externally. Please enable network requests in your
+              browser.
+            </p>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
