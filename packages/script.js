ab956608f1fe041cad25975da71cee5fcc8bc527
Lalit <lalitvijay9480@gmail.com>
2021-08-06 10:46:32 +0530

    fix: template strings

--- packages/script.js
+++ packages/script.js
@@ -22,7 +22,7 @@
 
   if (token) {
     const res = await fetch(
-      '${B_URL}/api/verify-token/?token=' + decodeURIComponent(token)
+      `${B_URL}/api/verify-token/?token=` + decodeURIComponent(token)
       // 'http://localhost:3000/api/verify-token/?token=' + token
     );
     const data = await res.json();
