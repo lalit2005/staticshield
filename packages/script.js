c81e368f7412ca001fa7abf2ed8133488a0de268
Arthur Denner <arthurdenner7@gmail.com>
2022-05-24 23:45:28 +0200

    feat: replace uglify-js with parcel to build script.js

--- packages/script.js
+++ packages/script.js
@@ -1,7 +1,7 @@
+import BASE_URL from '../lib/baseUrl';
+
 (async function () {
   const params = new URLSearchParams(window.location.search);
-  // const BASE_URL = 'http://localhost:3000';
-  const BASE_URL = 'https://staticshield.vercel.app';
   const tokenFromUrl = params.get('token');
   if (tokenFromUrl) {
     window.localStorage.setItem('staticshield-token', tokenFromUrl);
@@ -11,44 +11,41 @@
   const siteId = staticshieldsScript.getAttribute('data-site-id');
   const token = window.localStorage.getItem('staticshield-token');
 
-  if (token == null || undefined || !token) {
+  if (!token) {
     window.location.replace(
       `${BASE_URL}/p/${siteId}/?&redirecturl=${window.location.href
         .split('?')[0]
         .toString()}`
     );
+    return;
   }
 
-  if (token) {
-    const res = await fetch(
-      `https://staticshield.glitch.me/verify-token/?token=` +
-        decodeURIComponent(token)
-      // 'http://localhost:3000/api/verify-token/?token=' + token
+  const res = await fetch(
+    `${BASE_URL}/api/verify-token?token=` + decodeURIComponent(token)
+  );
+  const data = await res.json();
+  console.log(data);
+  if (data.expired == true) {
+    window.location.replace(
+      `${BASE_URL}/p/${siteId}/?expired=1&redirecturl=${window.location.href
+        .split('?')[0]
+        .toString()}`
     );
-    const data = await res.json();
-    console.log(data);
-    if (data.expired == true) {
-      window.location.replace(
-        `${BASE_URL}/p/${siteId}/?expired=1&redirecturl=${window.location.href
-          .split('?')[0]
-          .toString()}`
-      );
-    }
+  }
 
-    if (data.invalidtoken == true) {
-      window.location.replace(
-        `${BASE_URL}/p/${siteId}/?invalidtoken=1&&redirecturl=${window.location.href
-          .split('?')[0]
-          .toString()}`
-      );
-    }
-    if (data.invalidtoken == false && data.expired == false) {
-      window.staticshieldToken = localStorage.getItem('staticshield-token');
-      let asd = document
-        .querySelector('.staticshield-div')
-        .classList.toggle('staticshield-div');
-      console.log(asd);
-    }
+  if (data.invalidtoken == true) {
+    window.location.replace(
+      `${BASE_URL}/p/${siteId}/?invalidtoken=1&&redirecturl=${window.location.href
+        .split('?')[0]
+        .toString()}`
+    );
+  }
+  if (data.invalidtoken == false && data.expired == false) {
+    window.staticshieldToken = localStorage.getItem('staticshield-token');
+    let asd = document
+      .querySelector('.staticshield-div')
+      .classList.toggle('staticshield-div');
+    console.log(asd);
   }
 })();
 
